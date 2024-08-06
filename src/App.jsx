46c9df0ad866d6bfd5a6e3ce03b6dc349be93726
djpb <djpbadenhorst@gmail.com>
2024-08-06 10:09:00 +0200

    Minor gui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Logo from './assets/old_mutual_logo.jpg'; ///src/assets/old_mutual_logo.jpg 
 
 function App({props}) {
   const [advisor, setAdvisor] = useState("");
@@ -18,20 +19,25 @@ function App({props}) {
   return (
     <>
       <div className="h-screen">
-        <div className="fixed right-0 w-full h-1/4">
-          <div className="h-full w-11/12 content-center">
+        <div className="fixed left-0 w-1/12 h-1/4 p-4">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div className="fixed right-0 w-5/6 h-1/4">
+          <div className="h-full w-full content-center">
             <p className="text-4xl md:text-5xl lg:text-6xl font-bold text-white text-right">VAN ZYL & ASSOCIATES</p>
             <p className="text-xl md:text-2xl lg:text-3xl font-bold text-black text-right">Old Mutual Personal Financial Advice</p>
           </div>
         </div>
 
         <div className="h-screen content-center">
-          <div className="w-2/3 md:w-1/2 lg:w-1/3 m-auto flex justify-center border-2 border-black rounded-lg p-10 bg-black/[.6] shadow-2xl">
+          <div className="w-2/3 md:w-1/2 lg:w-1/2 m-auto flex justify-center border-2 border-black rounded-lg p-5 bg-white/[.8] shadow-2xl">
             <div className="flex-col space-y-5">
               <div>
-                <p className="text-sm lg:text-xl font-bold text-white text-left pb-4">
-                  Thank you, we received your consent letter. We will message you shortly with further information. <br/><br/>
-                  Dankie vir u konsent dokument, ons gaan binnekort u boodskap vir verdere inligting.
+                <p className="text-sm lg:text-lg font-bold text-black text-left pb-4">
+                  Thank you, we received your consent letter. We will message you shortly with further information. <br/>
+                  Beskou asb weer Whatsapp om die gesprek voort te sit. <br/><br/>
+                  Dankie vir u konsent dokument, ons gaan binnekort u boodskap vir verdere inligting.<br/>
+                  Please revert back to whatsapp to continue the conversation.
                 </p>
               </div>
               <div className="flex justify-center">
